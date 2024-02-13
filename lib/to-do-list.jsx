b3eefd963c7e2b4fc868f032f17e1facb4cfbcba
Rachael Momber <rlmomber@gmail.com>
2024-02-13 14:52:57 +0900

    localStorage capabilities now functional

--- lib/to-do-list.jsx
+++ lib/to-do-list.jsx
@@ -1,35 +1,35 @@
 // import { useState, useEffect } from 'react';
 
 const App = () => {
+  const data = localStorage.getItem('todos');
+  const storedTodos = JSON.parse(data);
 
-  // const storedTodos = JSON.parse(localStorage('todos'));
-  // // useState returns an array so we can save and destructure it by using const
-  // // and passing useState an empty array at first
-  // const [todos, changeTodos] = useState(storedTodos);
-
-  // // every time there is a change, the browser will track it
-  // // and change the array listed after the object
-  // useEffect(() => {
-  //   // localStorage expects a string so you must stringify your value first
-  //   localStorage.setItem('todos', JSON.stringify(todos));
-  //   console.log(todos);
-  // }, [todos])
-
-  const [todos, changeTodos] = React.useState(
-    [
+  const defaultTodos = [
       { title: "Code a to-do list", done: false },
       { title: "Eat breakfast", done: true },
       { title: "Do some exercise", done: false },
       { title: "Water the plants", done: true }
     ]
-  );
+
+  // if there is data in local storage, use it, otherwise use defaults
+  const input = data ? storedTodos : defaultTodos;
+  // useState returns an array so we can save and destructure it by using const
+  // and passing useState an empty array at first
+  const [todos, changeTodos] = React.useState(input);
+
+  // every time there is a change, the browser will track it
+  // and change the array listed after the object
+  React.useEffect(() => {
+    // localStorage expects a string so you must stringify your value first
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   const [title, setTitle] = React.useState('');
 
   const handleTitleChange = (event) => {
     // calls the method defined when the title array is defined
     setTitle(event.target.value);
   }
-  console.log(localStorage);
 
   const addTodo = () => {
     console.log('adding a to-do...');
